c1a03640ea534e50ab1948839de8a78da56c2c67
radaiko <a.radlingmayr@icloud.com>
2024-11-08 09:25:11 +0100

    add array and stream example

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,13 +9,30 @@ internal static class Program {
     private static Customer.CustomerClient _customerClient;
 
     private static async Task Main(string[] args) {
-        var channel = GrpcChannel.ForAddress("http://localhost:5000", new GrpcChannelOptions
-        {
+        // ---------------------------------------------------------------------
+        var channel = GrpcChannel.ForAddress("http://localhost:5000", new GrpcChannelOptions {
             Credentials = ChannelCredentials.Insecure
         });
         _customerClient = new Customer.CustomerClient(channel);
+        
+        // ---------------------------------------------------------------------
+        Console.WriteLine("Output existing customer");
         for (var i = 1; i <= 3; i++) {
             await OutputCustomer(i);
+        }
+        
+        // ---------------------------------------------------------------------
+        Console.WriteLine("Output new customers from stream");
+        var stream = _customerClient.GetNewCustomersAsStream(new Empty());
+        await foreach (var newCustomer in stream.ResponseStream.ReadAllAsync()) {
+            Console.WriteLine($"{newCustomer.Firstname}, {newCustomer.Lastname}");
+        }
+        
+        // ---------------------------------------------------------------------
+        Console.WriteLine("Output new customers from arary");
+        var newCustomerArray = await _customerClient.GetNewCustomersAsArrayAsync(new Empty());
+        foreach (var newCustomer in newCustomerArray.Customers) {
+            Console.WriteLine($"{newCustomer.Firstname}, {newCustomer.Lastname}");
         }
     }
 
--- Server/Services/CustomersService.cs
+++ Server/Services/CustomersService.cs
@@ -15,7 +15,8 @@ public class CustomersService : Customer.CustomerBase {
     if (request.Id == 1) {
       output.Firstname = "Jamie";
       output.Lastname = "Smith";
-    } else if (request.Id == 2) {
+    }
+    else if (request.Id == 2) {
       output.Firstname = "Alex";
       output.Lastname = "Johnson";
     }
@@ -25,5 +26,31 @@ public class CustomersService : Customer.CustomerBase {
     }
 
     return Task.FromResult(output);
+  }
+
+  public override Task<CustomersModel> GetNewCustomersAsArray(Empty request, ServerCallContext context) {
+    List<CustomerModel> newCustomers = [
+      new() { Firstname = "a", Lastname = "b" }, 
+      new() { Firstname = "c", Lastname = "d" }, 
+      new() { Firstname = "e", Lastname = "f" }
+    ];
+
+    CustomersModel output = new();
+    output.Customers.AddRange(newCustomers);
+    
+    return Task.FromResult(output);
+  }
+
+  public override async Task GetNewCustomersAsStream(Empty request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context) {
+    List<CustomerModel> newCustomers = [
+      new() { Firstname = "a", Lastname = "b" }, 
+      new() { Firstname = "c", Lastname = "d" }, 
+      new() { Firstname = "e", Lastname = "f" }
+    ];
+
+    foreach (var newCustomer in newCustomers) {
+      await responseStream.WriteAsync(newCustomer);
+    }
+    
   }
 }
